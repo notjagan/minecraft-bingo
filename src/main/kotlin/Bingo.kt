6373ef9faa40c6acc059babef61d9830bcf8ceee
Jagan Prem <notjagan@gmail.com>
2022-01-15 16:49:14 -0500

    Add bingo command executor

--- src/main/kotlin/Bingo.kt
+++ src/main/kotlin/Bingo.kt
@@ -1,8 +1,10 @@
+import command.BingoCommandExecutor
 import org.bukkit.plugin.java.JavaPlugin
 
 class Bingo : JavaPlugin() {
     override fun onEnable() {
         super.onEnable()
+        getCommand("bingo")?.setExecutor(BingoCommandExecutor(this))
     }
 
     override fun onDisable() {
---
+++ src/main/kotlin/command/BingoCommandExecutor.kt
@@ -0,0 +1,38 @@
+package command
+
+import game.Game
+import game.createGame
+import org.bukkit.ChatColor
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.plugin.Plugin
+
+class BingoCommandExecutor(private val plugin: Plugin) : CommandExecutor {
+    private var game: Game? = null
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (args.isEmpty())
+            return false
+        when (args[0]) {
+            "start" -> {
+                if (args.size > 1)
+                    return false
+                game = createGame(plugin)
+            }
+            "join" -> {
+                if (args.size > 1)
+                    return false
+                else if (sender !is Player)
+                    sender.sendMessage("${ChatColor.RED}Invalid player.")
+                else if (game == null)
+                    sender.sendMessage("${ChatColor.RED}No game in progress.")
+                else
+                    game?.addPlayer(sender)
+            }
+            else -> return false
+        }
+        return true
+    }
+}
