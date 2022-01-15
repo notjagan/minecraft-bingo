2b0a7194da505ed8fff76c1789e0ca2d209fec05
Jagan Prem <notjagan@gmail.com>
2022-01-15 12:43:12 -0500

    Add skeleton for plugin

---
+++ src/main/kotlin/Bingo.kt
@@ -0,0 +1,11 @@
+import org.bukkit.plugin.java.JavaPlugin
+
+class Bingo : JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
