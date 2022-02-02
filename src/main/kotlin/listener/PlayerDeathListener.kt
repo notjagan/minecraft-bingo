b85f14ef1a52f5a31ffc48f5edb53bc1793628fd
Jagan Prem <notjagan@gmail.com>
2022-02-01 19:48:06 -0500

    Add listener for death

---
+++ src/main/kotlin/listener/PlayerDeathListener.kt
@@ -0,0 +1,19 @@
+package listener
+
+import game.State
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.PlayerDeathEvent
+import util.Objective
+
+class PlayerDeathListener(
+    state: State,
+    objective: Objective,
+    private val player: Player
+) : ObjectiveListener(state, objective, player) {
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent) {
+        if (event.entity.uniqueId == player.uniqueId)
+            updateObjectiveStatus()
+    }
+}
