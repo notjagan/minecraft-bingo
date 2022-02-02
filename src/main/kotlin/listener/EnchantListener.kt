9d9d1e74cc24febce223671c54977ae65a4a0b71
Jagan Prem <notjagan@gmail.com>
2022-02-01 21:18:09 -0500

    Add listener for enchanting

---
+++ src/main/kotlin/listener/EnchantListener.kt
@@ -0,0 +1,27 @@
+package listener
+
+import game.State
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.enchantment.EnchantItemEvent
+import util.Objective
+
+class EnchantListener(
+    state: State,
+    objective: Objective,
+    private val player: Player,
+    private val item: Material
+) : ObjectiveListener(state, objective, player) {
+    @EventHandler
+    fun onEnchant(event: EnchantItemEvent) {
+        if (event.item.type == item && event.enchanter.uniqueId == player.uniqueId)
+            updateObjectiveStatus()
+    }
+
+    companion object {
+        fun factory(item: Material) = { state: State, objective: Objective, player: Player ->
+            EnchantListener(state, objective, player, item)
+        }
+    }
+}
