6756a68f99966f5b24722ccabe8fede0ead78b4a
Jagan Prem <notjagan@gmail.com>
2022-02-01 19:47:48 -0500

    Add listener for block place

---
+++ src/main/kotlin/listener/PlaceListener.kt
@@ -0,0 +1,27 @@
+package listener
+
+import game.State
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.BlockPlaceEvent
+import util.Objective
+
+class PlaceListener(
+    state: State,
+    objective: Objective,
+    private val player: Player,
+    private val block: Material
+) : ObjectiveListener(state, objective, player) {
+    @EventHandler
+    fun onPlace(event: BlockPlaceEvent) {
+        if (event.block.type == block && event.player.uniqueId == player.uniqueId)
+            updateObjectiveStatus()
+    }
+
+    companion object {
+        fun factory(block: Material) = { state: State, objective: Objective, player: Player ->
+            PlaceListener(state, objective, player, block)
+        }
+    }
+}
