3760e3d849b340f9d78cb59970608edb3a504768
Jagan Prem <notjagan@gmail.com>
2022-02-01 20:25:50 -0500

    Add listener for crafting

---
+++ src/main/kotlin/listener/CraftListener.kt
@@ -0,0 +1,27 @@
+package listener
+
+import game.State
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.inventory.CraftItemEvent
+import util.Objective
+
+class CraftListener(
+    state: State,
+    objective: Objective,
+    private val player: Player,
+    private val material: Material
+) : ObjectiveListener(state, objective, player) {
+    @EventHandler
+    fun onCraft(event: CraftItemEvent) {
+        if (event.recipe.result.type == material && event.whoClicked.uniqueId == player.uniqueId)
+            updateObjectiveStatus()
+    }
+
+    companion object {
+        fun factory(material: Material) = { state: State, objective: Objective, player: Player ->
+            CraftListener(state, objective, player, material)
+        }
+    }
+}
